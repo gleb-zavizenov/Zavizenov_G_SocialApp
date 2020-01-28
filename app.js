445ee1cc38002ba0e697b92f0f733092333fd3f8
gleb-zavizenov <g_zavizenov@fanshaweonline.ca>
2020-01-28 17:47:35 -0500

    Added messages

--- app.js
+++ app.js
@@ -26,6 +26,13 @@ io.on('connection', function(socket) {
     console.log("user is connected!");
     socket.emit('connected', {sID: `${socket.id}`, message: 'new connection'});
 
+    socket.on('chat_message', function(msg){
+        console.log(msg);
+        
+        //when we get a message, we send it to everyone
+        io.emit('new_message', {id: socket.id, message: msg});
+    })
+
     // listen for a disconnect event
     socket.on('disconnect',function(){
         console.log('a user is disconnected');
